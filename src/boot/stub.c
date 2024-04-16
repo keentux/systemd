3d0ec4367a9562b821887882221f2653f0cdf6f9
Valentin Lefebvre <valentin.lefebvre@suse.com>
2024-04-16 11:00:46 +0200

    load efi addons just one time
    - During the loading addons process, efi addons is load twice.
      As efi addons are kind of global addons, this change load efi
      ones with them.
    
    Signed-off-by: Valentin Lefebvre <valentin.lefebvre@suse.com>

--- src/boot/stub.c
+++ src/boot/stub.c
@@ -622,6 +622,7 @@ static void items_free(char16_t **items, size_t n_items) {
 static EFI_STATUS load_addons(
                 EFI_HANDLE stub_image,
                 EFI_LOADED_IMAGE_PROTOCOL *loaded_image,
+                bool load_from_efi,
                 const char16_t *prefix,
                 const char *uname,
                 char16_t **cmdline,                         /* Both input+output, extended with new addons we find */
@@ -659,9 +660,11 @@ static EFI_STATUS load_addons(
         if (err != EFI_SUCCESS)
                 return err;
 
-        err = load_addons_from_efi(loaded_image, &addons, &n_items, &n_allocated);
-        if (err != EFI_SUCCESS)
-                return err;
+        if (load_from_efi) {
+                err = load_addons_from_efi(loaded_image, &addons, &n_items, &n_allocated);
+                if (err != EFI_SUCCESS)
+                        return err;
+        }
 
         if (n_items == 0)
                 return EFI_SUCCESS; /* Empty directory */
@@ -1108,6 +1111,7 @@ static void load_all_addons(
         err = load_addons(
                         image,
                         loaded_image,
+                        true,
                         u"\\loader\\addons",
                         uname,
                         cmdline_addons,
@@ -1128,6 +1132,7 @@ static void load_all_addons(
         err = load_addons(
                         image,
                         loaded_image,
+                        false,
                         dropin_dir,
                         uname,
                         cmdline_addons,
